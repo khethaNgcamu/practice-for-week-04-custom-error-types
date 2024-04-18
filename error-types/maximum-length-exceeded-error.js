522ced2610d174176f2cdba57efe5902e24d4f00
Smanga Ngcamu <ngcamukhetha@gamil.com>
2024-04-18 16:51:38 +0200

    custom error type completed

--- error-types/maximum-length-exceeded-error.js
+++ error-types/maximum-length-exceeded-error.js
@@ -1,6 +1,23 @@
 const ValidationError = require('./validation-error');
 
 // Your code here
+// maximum-length-exceeded-error.js
+class MaximumLengthExceededError extends ValidationError {
+  constructor(excessLength, ...params) {
+    super(...params);
+
+    // if (Error.captureStackTrace) {
+    //   Error.captureStackTrace(this, MaximumLengthExceededError); // Ensure this line is only called once
+    // }
+
+    this.name = 'MaximumLengthExceededError';
+    this.message = excessLength ? `Maximum length exceeded by ${excessLength}` : 'Maximum length exceeded';
+  }
+}
+
+
+
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- error-types/validation-error.js
+++ error-types/validation-error.js
@@ -1,4 +1,17 @@
 // Your code here
+class ValidationError extends Error {
+  constructor(difference = 'Invalid input', ...params) {
+    super(...params);
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ValidationError);
+    }
+
+    this.name = 'ValidationError';
+    this.difference = difference;
+    this.message = `${difference}`;
+  }
+}
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
